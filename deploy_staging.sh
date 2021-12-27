75ed1f85d4ad9b58c5b34ed7deb815165223cfa5
Alexandre Karpov <alexakarpov@gmail.com>
2021-12-26 20:49:00 -0500

    local switch to pg

--- deploy_staging.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/bash
-
-pyclean () {
-    find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
-}
-
-pyclean
-zip -qr htmshop.zip ecommerce appspec.yml requirements.txt manage.py static templates
-scp htmshop.zip transylvania.bostonmonks.com:~
--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -1,25 +1,31 @@
-from .common import *
-from pathlib import Path
-from dotenv import dotenv_values
 import os
+from pathlib import Path
 
-ENV = 'development'
+from dotenv import dotenv_values
 
-print("workdir: ",  os.getcwd())
+from .common import *
+
+ENV = "development"
+
+print("workdir: ", os.getcwd())
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 print(f"BASE_DIR: {BASE_DIR}")
 
-config = dotenv_values("devops/secrets/.env")
+config = dotenv_values("devops/secrets/development.env")
 SECRET_KEY = config["SECRET_KEY"]
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config["POSTGRES_DB"],
+        "USER": config["POSTGRES_USER"],
+        "PASSWORD": config["POSTGRES_PASSWORD"],
+        "HOST": "localhost",
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
@@ -28,39 +34,33 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / "media/"
 
 LOGGING = {
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
+    "version": 1,
+    "formatters": {
+        "verbose": {"format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"},
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "simple"},
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
         },
     },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    }
 }
 
-STRIPE_PUBLISHABLE_KEY = "pk_test_51K6PnSCFIch5ayqknnamJA9P1lEBHAft8RXXVPKYIEtciJKZbBRoS7Rt1mfbtFof1VCHdEd0Ax5OovOZDxRnz0QA00YsK7ehAb"
+STRIPE_PUBLISHABLE_KEY = (
+    "pk_test_51K6PnSCFIch5ayqknnamJA9P1lEBHAft8RXXVPKYIEtciJKZbBRoS7Rt1mfbtFof1VCHdEd0Ax5OovOZDxRnz0QA00YsK7ehAb"
+)
 STRIPE_SECRET_KEY = config["STRIPE_SECRET_KEY"]
 
 
 if DEBUG:
     # make all loggers use the console.
-    for logger in LOGGING['loggers']:
-        LOGGING['loggers'][logger]['handlers'] = ['console']
+    for logger in LOGGING["loggers"]:
+        LOGGING["loggers"][logger]["handlers"] = ["console"]
