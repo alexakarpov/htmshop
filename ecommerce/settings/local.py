e2fe20f499f91cb73a624cff6ad72e399673bd98
Alexandre Karpov <alexakarpov@gmail.com>
2021-12-27 22:41:54 -0500

    back to sqlite - tests

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -16,16 +16,22 @@ SECRET_KEY = config["SECRET_KEY"]
 DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": config["POSTGRES_DB"],
+#         "USER": config["POSTGRES_USER"],
+#         "PASSWORD": config["POSTGRES_PASSWORD"],
+#         "HOST": "localhost",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config["POSTGRES_DB"],
-        "USER": config["POSTGRES_USER"],
-        "PASSWORD": config["POSTGRES_PASSWORD"],
-        "HOST": "localhost",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = []
 
@@ -64,3 +70,29 @@ if DEBUG:
     # make all loggers use the console.
     for logger in LOGGING["loggers"]:
         LOGGING["loggers"][logger]["handlers"] = ["console"]
+
+    # django-debug-toolbar
+
+    INTERNAL_IPS = ("127.0.0.1",)
+    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+    INSTALLED_APPS += ("debug_toolbar",)
+
+    DEBUG_TOOLBAR_PANELS = [
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.logging.LoggingPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "INTERCEPT_REDIRECTS": False,
+    }
