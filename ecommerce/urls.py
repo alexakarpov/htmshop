b97c3eb9443993430b157d37ea39b2ee1d68fa37
Alexandre Karpov <alexakarpov@gmail.com>
2021-12-27 22:43:15 -0500

    debug toolbar and fresh init migrations

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -10,6 +11,7 @@ urlpatterns = [
     path("basket/", include("ecommerce.apps.basket.urls", namespace="basket")),
     path("account/", include("ecommerce.apps.account.urls", namespace="account")),
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
