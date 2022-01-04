09677da1fce472097f480c518e9314d74a33057b
Alexandre Karpov <alexakarpov@gmail.com>
2022-01-04 14:38:56 -0500

    paypal creds read from config/settings

--- ecommerce/apps/checkout/paypal.py
+++ ecommerce/apps/checkout/paypal.py
@@ -1,11 +1,13 @@
 import sys
 
+from django.conf import settings
 from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 
 
 class PayPalClient:
     def __init__(self):
-        self.client_id = ""
-        self.client_secret = ""
+        self.client_id = settings.PAYPAL_CLIENT_ID
+        self.client_secret = settings.PAYPAL_SECRET
+        # TODO: do something with _production_ environment
         self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
         self.client = PayPalHttpClient(self.environment)
