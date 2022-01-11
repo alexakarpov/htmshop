5502aa1572b0f136dff0ff2a53fbc06359b6002f
Alexandre Karpov <alexakarpov@gmail.com>
2022-01-10 23:48:04 -0500

    reorder_urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,12 +6,12 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("ecommerce.apps.catalogue.urls", namespace="catalogue")),
     path("checkout/", include("ecommerce.apps.checkout.urls", namespace="checkout")),
     path("basket/", include("ecommerce.apps.basket.urls", namespace="basket")),
     path("account/", include("ecommerce.apps.account.urls", namespace="account")),
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
     path("__debug__/", include(debug_toolbar.urls)),
+    path("", include("ecommerce.apps.catalogue.urls", namespace="catalogue")),
 ]
 
 if settings.DEBUG:
