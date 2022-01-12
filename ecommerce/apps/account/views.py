8612872f6964b6c20d62b68b3dc3f5c97234147a
Alexandre Karpov <alexakarpov@gmail.com>
2022-01-12 13:39:03 -0500

    django 4 only change

--- ecommerce/apps/account/views.py
+++ ecommerce/apps/account/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.urls import reverse
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from ecommerce.apps.catalogue.models import Product
 from ecommerce.apps.orders.models import Order
@@ -108,7 +108,7 @@ def account_register(request):
 
 def account_activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = Customer.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, user.DoesNotExist):
         user = None
