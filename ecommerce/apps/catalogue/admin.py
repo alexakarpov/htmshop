bcf8c4df4ef44169c55f55c2b7b7c6a23b6c52a7
Alexandre Karpov <alexakarpov@gmail.com>
2022-03-02 23:06:39 -0500

    ditto

--- ecommerce/apps/catalogue/admin.py
+++ ecommerce/apps/catalogue/admin.py
@@ -4,23 +4,23 @@ from django.contrib import admin
 from .models import (
     Category,
     Product,
+    ProductAttribute,
+    ProductAttributeValue,
     ProductImage,
-    ProductSpecification,
-    ProductSpecificationValue,
     ProductType,
 )
 
 admin.site.register(Category)
 
 
-class ProductSpecificationInline(admin.TabularInline):
-    model = ProductSpecification
+class ProductAttributeInline(admin.TabularInline):
+    model = ProductAttribute
 
 
 @admin.register(ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
     inlines = [
-        ProductSpecificationInline,
+        ProductAttributeInline,
     ]
 
 
@@ -28,13 +28,13 @@ class ProductImageInline(admin.TabularInline):
     model = ProductImage
 
 
-class ProductSpecificationValueInline(admin.TabularInline):
-    model = ProductSpecificationValue
+class ProductAttributeValueInline(admin.TabularInline):
+    model = ProductAttributeValue
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [
-        ProductSpecificationValueInline,
+        ProductAttributeValueInline,
         ProductImageInline,
     ]
