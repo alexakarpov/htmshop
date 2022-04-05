193ba9c0715314e4d203847de59c6a41b748aa40
Alexandre Karpov <alexakarpov@gmail.com>
2022-04-05 06:36:04 -0400

    books_no_variants

--- ecommerce/apps/shipping/utils.py
+++ ecommerce/apps/shipping/utils.py
@@ -2,7 +2,5 @@ def variants(product_type):
     print(f"retieving variants for {product_type}")
     if product_type.name == "icons":
         return ["8x10", "wallet-size"]
-    elif product_type.name == "books":
-        return ["qwerty", "asdfgh"]
     else:
         return []
