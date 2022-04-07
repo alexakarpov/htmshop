155c5ae0fb05075de6a7ed596d8bf67d43ecae57
Alexandre Karpov <alexakarpov@gmail.com>
2022-04-07 00:22:54 -0400

    remove delivery choices route

--- ecommerce/apps/checkout/urls.py
+++ ecommerce/apps/checkout/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "checkout"
 
 urlpatterns = [
-    path("deliverychoices", views.deliverychoices, name="deliverychoices"),
     path("basket_update_delivery/", views.basket_update_delivery, name="basket_update_delivery"),
     path("delivery_address/", views.delivery_address, name="delivery_address"),
     path("payment_selection/", views.payment_selection, name="payment_selection"),
