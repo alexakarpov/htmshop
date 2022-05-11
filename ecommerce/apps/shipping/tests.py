84edf5018bdaa5945cb18f1c9c7cbf02bf96d30b
Alexandre Karpov <alexakarpov@gmail.com>
2022-05-10 22:17:12 -0400

    and tests thereof

--- ecommerce/apps/shipping/tests.py
+++ ecommerce/apps/shipping/tests.py
@@ -1,5 +1,9 @@
-from django.test import TestCase
+import unittest
 
+from django.test import TestCase
+from ecommerce.apps.accounts.models import Address
+
+from .engine import make_shipment
 from .utils import get_weight
 
 # # Create your tests here.
@@ -11,3 +15,19 @@ from .utils import get_weight
 #         }
 #     }
 #     assert get_weight(basket) == 32
+
+
+def test_make_shipment():
+    b = {}
+    a = Address()
+    a.full_name = "John Doe"
+    a.address_line = "1 Main St"
+    a.postcode = "98765"
+
+    s = make_shipment(b, a)
+    sd = s["shipment"]
+    assert "usps_parcel_select" in s["rate_options"]["service_codes"]
+    assert sd["ship_from"]["company_name"] == "Holy Transfiguration Monastery"
+    assert sd["ship_to"]["postal_code"] == a.postcode
+    assert sd["ship_to"]["country_code"] == a.country == "US"
+    assert sd.get("packages") == []
