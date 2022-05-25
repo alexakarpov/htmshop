6c1a7745202a6ebcdfa8b573304e7820d3ad1943
Alexandre Karpov <alexakarpov@gmail.com>
2022-05-25 18:05:42 -0400

    tests_update

--- ecommerce/apps/shipping/tests.py
+++ ecommerce/apps/shipping/tests.py
@@ -1,20 +1,13 @@
+import imp
+import json
 import unittest
 
 from django.test import TestCase
 from ecommerce.apps.accounts.models import Address
 
+from .choice import rate_to_choice
 from .engine import make_shipment
 from .utils import get_weight
-
-# # Create your tests here.
-# def test_get_weight():
-#     basket = {
-#         "basket": {
-#             "2": {"price": "30.00", "qty": 1, "variant": "8x10", "title": "Holy Napkin", "weight": 16},
-#             "1": {"price": "20.00", "qty": 2, "variant": "", "title": "Prayer Book", "weight": 8},
-#         }
-#     }
-#     assert get_weight(basket) == 32
 
 
 def test_make_shipment():
@@ -29,8 +22,15 @@ def test_make_shipment():
 
     s = make_shipment(b, a)
     sd = s["shipment"]
-    assert "usps_parcel_select" in s["rate_options"]["service_codes"]
     assert sd["ship_from"]["company_name"] == "Holy Transfiguration Monastery"
     assert sd["ship_to"]["postal_code"] == a.postcode
     assert sd["ship_to"]["country_code"] == a.country == "US"
     assert sd.get("packages") == [{"weight": {"value": 24, "unit": "ounce"}}]
+
+
+def test_rate_to_choice():
+    with open("shipping_jsons/rate.json", "r") as f:
+        rate = json.load(f)
+        choice = rate_to_choice(rate)
+        assert choice.price == 17.74
+        assert choice.name == "usps_priority_mail"
