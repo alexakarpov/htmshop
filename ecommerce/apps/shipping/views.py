ae632200c738858d7ce4c71d26fd678b27bf7b44
Alexandre Karpov <alexakarpov@gmail.com>
2022-06-09 23:05:08 -0400

    shipping API kinda working

--- ecommerce/apps/shipping/views.py
+++ ecommerce/apps/shipping/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from ecommerce.apps.accounts.models import Address
+from ecommerce.apps.basket.basket import Basket
+from ecommerce.apps.shipping.choice import ShippingChoice
+
+# from ecommerce.apps.shipping.choice import ShippingChoice, ShippingChoiceEncoder
+from ecommerce.apps.shipping.engine import shipping_choices
+from ecommerce.utils import debug_print
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import ShippingChoiceSerializer
+
+
+@api_view(http_method_names=["GET"])
+def get_rates(request):
+  basket = Basket(request)
+  session = request.session
+  address_id = session["address"]["address_id"]
+  address = Address.objects.get(id=address_id)
+  choices = shipping_choices(basket, address)
+  serializer = ShippingChoiceSerializer(choices, many=True)
+  sdata=serializer.data
+  return JsonResponse({"choices": sdata})
