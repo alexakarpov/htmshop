9e8d6dd1acce51848f00a1b44c3ec57ac7f5ef93
Alex Karpov <alexakarpov@gmail.com>
2022-06-15 22:26:24 -0400

    fix deployment process:

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,10 @@ echo you are on $BRANCH
 if [ $BRANCH == 'main' ]; then
   find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
   rm -f htmshop.zip
+  poetry export -f requirements.txt --without-hashes > requirements.txt
   zip -qr htmshop.zip ecommerce requirements.txt manage.py static templates fixtures
   scp htmshop.zip transylvania.bostonmonks.com:/tmp/
+  rm requirements.txt
   ansible-playbook -i devops/inventory.ini devops/playbooks/deploy.yml
 else
   echo You must be on release branch
