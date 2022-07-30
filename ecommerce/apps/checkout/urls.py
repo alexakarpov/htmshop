b3f9a97f4b5f9bba5b64ea6d1d0106ab2dec1ab3
Alex Karpov <alexakarpov@gmail.com>
2022-07-29 23:54:54 -0400

    payment for POC, still failing, and url

--- ecommerce/apps/checkout/urls.py
+++ ecommerce/apps/checkout/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("payment_complete/", views.payment_complete, name="payment_complete"),
     path("payment_successful/", views.payment_successful,
          name="payment_successful"),
-    path('payment/', views.payment_with_token, name="process_square_payment")
+    path('payment/', views.payment_with_token, name="token_payment"),
 ]
