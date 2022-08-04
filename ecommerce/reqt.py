8e7e2996604dc3498262289859a0db48f4faf3dc
Alexandre Karpov <alexakarpov@gmail.com>
2022-08-03 22:05:07 -0400

    starting on pay request

--- ecommerce/reqt.py
+++ ecommerce/reqt.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python
 
+from types import CoroutineType
 import requests
 
 print(requests.__version__)
+POST_URL = 'https://secure.networkmerchants.com/api/transact.php'
+
+SALE = 'sale'
+SECURITY_KEY = ''
+amount = '11'
+descriptor = "something"
+tax = '-1'
+# shipping = ''  # total shipping amount?
+# ponumber = ''  # orignal order #
+# first_name = ''
+# last_name = ''
+
+# address_1
+# address_2
+# city
+# state
+# zip
+# country# phone
+# email
