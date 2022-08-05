cbea231ca23cb1fce26e105d4b418e9ce4201bc5
Alex Karpov <alexakarpov@gmail.com>
2022-08-05 01:32:55 -0400

    POC api POST

--- ecommerce/reqt.py
+++ ecommerce/reqt.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 
-from types import CoroutineType
 import requests
+from dotenv import dotenv_values
 
-print(requests.__version__)
+config = dotenv_values()
+
 POST_URL = 'https://secure.networkmerchants.com/api/transact.php'
+QUERY_URL = 'https://secure.networkmerchants.com/api/query.php'
 
 SALE = 'sale'
-SECURITY_KEY = ''
+
+SECURITY_KEY = config["STAX_SECURITY_KEY"]
 amount = '11'
 descriptor = "something"
 tax = '-1'
@@ -23,3 +26,41 @@ tax = '-1'
 # zip
 # country# phone
 # email
+
+
+# response = 3
+# &responsetext = Authentication Failed
+# &authcode =
+# &transactionid = 0
+# &avsresponse =
+# &cvvresponse =
+# &orderid =
+# &type =
+# &response_code = 300
+
+def report(res_text):
+
+    for i in res_text.split('&'):
+        (k, v) = i.split('=')
+        print(f"{k} => {v}")
+
+
+if __name__ == "__main__":
+    response = requests.post(
+        POST_URL,
+        params={'security_key': SECURITY_KEY,
+                'amount': '12.99',
+                'type': SALE,
+                'payment_token': '00000000-000000-000000-000000000000'
+                },
+        headers={},
+    )
+
+    if response:
+        print("SUCCESS")
+        print(response.headers)
+        print("----------------------------------")
+        report(response.text)
+    else:
+        print("ERROR")
+        report(response.text)
