a6cbd7ff4698d314cbe81ca3283193c32b7268f9
Alex Karpov <alexakarpov@gmail.com>
2022-08-05 23:23:27 -0400

    unused

--- devops/migrate.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/local/bin/bash
-ansible htmstore_dev -i inventory.ini -m community.general.django_manage -a "virtualenv=/home/ubuntu/htmshop_django_depl/venv project_path=/home/ubuntu/htmshop_django_depl command=migrate"
--- devops/restart.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/local/bin/bash
-ansible htmstore_dev -i inventory.ini -m systemd -a "service=gunicorn state=restarted" -K
