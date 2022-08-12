3eb0797766e4fceb47b9d086bcb0b7c2ac9009d2
Alexandre Karpov <alexakarpov@gmail.com>
2022-08-12 01:33:28 -0400

    Collect.js out of base

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('a first spec', () => {
-  it('has our title', () => {
+  it('has our title and a #cart', () => {
       cy.visit('localhost:8000')
       cy.title().should('eq', 'HTM Store')
+      cy.get("#cart").click({force: true}).get(".h2")
   })
 })
