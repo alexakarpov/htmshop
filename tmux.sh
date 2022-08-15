620d22498fabf0b9ecfff7806f042705bcb80095
Alex Karpov <alexakarpov@gmail.com>
2022-08-14 23:05:06 -0400

    script to start a session, unless already exists, with cypress and runserver

---
+++ tmux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SESSIONNAME="devservers"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ] 
+ then
+    tmux new -s $SESSIONNAME -d
+    tmux split-window -h -t $SESSIONNAME
+    window=${session}:0
+    pane=${window}.0
+    tmux send-keys -t "$pane" C-b 'poetry run ./manage.py runserver' Enter
+    pane=${window}.1
+    tmux select-pane -t "$pane"
+    tmux send-keys -t "$pane" C-b 'npx cypress open' Enter
+fi
+
+tmux attach -t $SESSIONNAME
