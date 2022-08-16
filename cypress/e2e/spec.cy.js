d34d64b525c9bd89d96ca5c44dc99924c938c1e5
Alexandre Karpov <alexakarpov@gmail.com>
2022-08-16 00:23:08 -0400

    second spec in same file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,24 @@
 describe('a first spec', () => {
-  it('has our title and a #cart', () => {
+  beforeEach(() => {
+    cy.visit('localhost:8000')
+  })
+
+  it('has our title', () => {
+    cy.title().should('eq', 'HTM Store')
+  })
+
+  it('a #cart redirecting to /basket/', () => {
+    // force - because "it's covered by another element (Django Debug Toolbar)
+    cy.get("#cart").click({force: true})
+                   .get(".h2")
+                   .contains('Your Cart')
+                   .url().should('include', '/basket/')
+  })
+})
+
+describe('wheee, a second spec', () => {
+  it('another spec', () => {
       cy.visit('localhost:8000')
       cy.title().should('eq', 'HTM Store')
-      cy.get("#cart").click({force: true}).get(".h2")
   })
 })
