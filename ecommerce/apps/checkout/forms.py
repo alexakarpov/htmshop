598e21c7c0a91db5994d115fa988b8ea0ccd0637
Alex Karpov <alexakarpov@gmail.com>
2022-08-31 22:18:50 -0400

    drop stax pay form

--- ecommerce/apps/checkout/forms.py
+++ ecommerce/apps/checkout/forms.py
@@ -4,27 +4,27 @@ from django.forms.widgets import TextInput
 from django.utils.translation import gettext_lazy as _
 
 
-class StaxPaymentForm(forms.Form):
-    cc_number = forms.CharField(
-        label="CC number",
-        required=True,
-        max_length=19,
-        error_messages={"required": "You will need a CC number"},
-    )
+# class StaxPaymentForm(forms.Form):
+#     cc_number = forms.CharField(
+#         label="CC number",
+#         required=True,
+#         max_length=19,
+#         error_messages={"required": "You will need a CC number"},
+#     )
 
-    cc_firstname = forms.CharField(
-        label="First name",
-        required=True,
-        max_length=5,
-        help_text="Required",
-        error_messages={
-            "required": "Sorry, you will need a CC first name"},
-    )
+#     cc_firstname = forms.CharField(
+#         label="First name",
+#         required=True,
+#         max_length=5,
+#         help_text="Required",
+#         error_messages={
+#             "required": "Sorry, you will need a CC first name"},
+#     )
 
-    cc_lastname = forms.CharField(
-        label="Last name",
-        required=True,
-        max_length=3,
-        help_text="Required",
-        error_messages={"required": "Sorry, you will need a CC last name"},
-    )
+#     cc_lastname = forms.CharField(
+#         label="Last name",
+#         required=True,
+#         max_length=3,
+#         help_text="Required",
+#         error_messages={"required": "Sorry, you will need a CC last name"},
+#     )
