ffe32fb7f07f700db4bc957fdc8e17d5d25bdbb7
Alex Karpov <alexakarpov@gmail.com>
2022-09-13 00:35:22 -0400

    little cleanup

--- ecommerce/apps/accounts/tests.py
+++ ecommerce/apps/accounts/tests.py
@@ -16,9 +16,10 @@ class AccountTest(TestCase):
     def test_address_from_json(self):
         address = Address.objects.get(
             pk='802596f0-1d01-4770-9a1c-bc453bcd6668')
-        j_str = address.toJSON()  # str
-        j = json.loads(j_str)
+        j_str = address.toJSON()
+        j_dict = json.loads(j_str)
         acc = Account.objects.get(email='alexandre.karpov@protonmail.com')
         self.assertEquals(address.customer, acc)
-        self.assertEquals(j["name"], "John Doe", "wrong name on the address")
-        self.assertEquals(j["city_locality"], "San Francisco")
+        self.assertEquals(j_dict["full_name"], "John Doe",
+                          "wrong name on the address")
+        self.assertEquals(j_dict["city_locality"], "San Francisco")
