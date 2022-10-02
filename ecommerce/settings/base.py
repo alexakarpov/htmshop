fdccd38dc546cf90d9a2ea7517683a0eb2e4b217
Alex Karpov <alexakarpov@gmail.com>
2022-10-01 23:57:12 -0400

    drop inventory from settings

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     "ecommerce.apps.checkout",
     "ecommerce.apps.shipping",
     "ecommerce.apps.playground",
-    "ecommerce.apps.inventory",
     "rest_framework",
     # "djpaypal",
 ]
