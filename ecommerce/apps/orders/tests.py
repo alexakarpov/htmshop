ca3883b283024b7a3b5cce3d4fb27fa83a0c1685
Alexandre Karpov <alexakarpov@gmail.com>
2022-11-12 14:36:41 -0500

    orders first test

--- ecommerce/apps/orders/tests.py
+++ ecommerce/apps/orders/tests.py
@@ -2,14 +2,48 @@ import json
 
 from django.test import TestCase
 
-from .models import Order, OrderItem
+from .models import Order, OrderItem, make_order
 
 
 class OrdersTest(TestCase):
-    fixtures = ['accounts.json', ]
+    fixtures = [
+        "accounts.json",
+    ]
 
     def test_order_from_dicts(self):
-        address_dict = ''
-        basket_dict = ''
+        test_cart_d = {
+            "skuxx": {
+                "price": "30.00",
+                "qty": 2,
+                "variant": "8x10",
+                "title": "Holy Napkin",
+                "weight": 16,
+            },
+            "skuyy": {
+                "price": "20.00",
+                "qty": 1,
+                "title": "Prayer Book",
+                "weight": 8,
+            },
+        }
 
-        self.assertEquals(2+2,5)
+        test_address_d = {
+            "full_name": "Testy Testo",
+            "address_line1": "1 qwe st",
+            "address_line2": "",
+            "phone": "1231231234",
+            "city_locality": "Qwepolis",
+            "state_province": "SC",
+            "postal_code": "1231234",
+            "country_code": "US",
+        }
+
+        order = make_order(
+            test_address_d, test_cart_d, "testytesto@example.com"
+        )
+
+        order.save()
+
+        self.assertEquals(order.full_name, "Testy Testo")
+        self.assertEquals(order.email, "testytesto@example.com")
+        self.assertEquals(order.total_paid, 2 * 30 + 20)
