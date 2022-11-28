41266fc8c230a6a084cecf91ff5b827085ff2f38
Alexandre Karpov <alexakarpov@gmail.com>
2022-11-28 13:21:50 -0500

    added playground with base2

--- ecommerce/apps/catalogue/urls.py
+++ ecommerce/apps/catalogue/urls.py
@@ -7,5 +7,6 @@ app_name = "catalogue"
 urlpatterns = [
     path("", views.catalogue_index, name="store_home"),
     path("product/<slug:slug>", views.product_detail, name="product_detail"),
+    path("playground/", views.playground, name="playground"),
     path("<slug:category_slug>/", views.category_list, name="category_list"),
 ]
