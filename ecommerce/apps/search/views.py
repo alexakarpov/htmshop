bd7a148bbd53a214a6c32b327d530f09d681e27d
Alexandre Karpov <alexakarpov@gmail.com>
2022-12-10 01:09:57 -0500

    search wip

--- ecommerce/apps/search/views.py
+++ ecommerce/apps/search/views.py
@@ -5,12 +5,14 @@ import logging
 
 logger = logging.getLogger("console")
 
-
 def search(request):
     products = Product.objects.all()
+    logger.debug(f"{products.count()} productsz in total")
+    logger.debug(f"request.GET is: {request.GET}")
     query = request.GET.get("q")
+    logger.debug(f"received query: {query}")
     if query:
         lookup = Q(title__icontains=query) | Q(description__icontains=query)
         products=products.filter(lookup).distinct()
-
+    logger.debug(f"{products.count()} products matched the query")
     return render(request, "search/index.html", {"products": products})
