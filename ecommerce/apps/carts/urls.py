2411efad62e98bc1c7e39e8e65181ad2938b7d0c
Alexandre Karpov <alexakarpov@gmail.com>
2022-12-22 00:21:20 -0500

    kill carts again =)

--- ecommerce/apps/carts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "carts"
-
-urlpatterns = [
-    path("", views.cart_home, name="cart_home")
-]
--- ecommerce/apps/carts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def cart_home(request):
-    return render(request, "carts/home.html", {})
