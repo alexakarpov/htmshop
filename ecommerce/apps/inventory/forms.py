e5cdd206c67a891f375ada0588fbfe8bf4d0e6af
Alexandre Karpov <alexakarpov@gmail.com>
2023-02-09 01:03:45 -0500

    quantity class with sizing

--- ecommerce/apps/inventory/forms.py
+++ ecommerce/apps/inventory/forms.py
@@ -10,7 +10,11 @@ ROOMS = [(1, "Sanding room"), (2, "Mounting room"), (3, "Printing room")]
 class MoveStockForm(forms.Form):
     from_room = forms.ChoiceField(label="From", choices=ROOMS)
     to_room = forms.ChoiceField(label="To", choices=ROOMS)
-    qty = forms.IntegerField(label="Quantity", min_value=0)
+    qty = forms.IntegerField(
+        label="Quantity",
+        min_value=0,
+        widget=forms.NumberInput(attrs={"class": "quantity"}),
+    )
     sku = forms.ModelChoiceField(
         queryset=ProductInventory.objects.filter(
             product_type__name="mounted icon"
