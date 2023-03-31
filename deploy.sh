3f79370f9a4cfe3635af7083ee0ab44696a96f13
Father Ephraim <frephraim@thehtm.org>
2023-03-30 22:25:52 -0400

    deploy with git

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 echo you are on $BRANCH
 
 if [ $BRANCH == 'release' ]; then
-  find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
-  rm -f htmshop.zip
-  poetry export -f requirements.txt --with dev --without-hashes > requirements.txt
-  zip -qr htmshop.zip ecommerce requirements.txt manage.py static templates
-  scp htmshop.zip transylvania.bostonmonks.com:/tmp/
-  rm requirements.txt
+  # find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+  # rm -f htmshop.zip
+  # poetry export -f requirements.txt --with dev --without-hashes > requirements.txt
+  # zip -qr htmshop.zip ecommerce requirements.txt manage.py static templates
+  # scp htmshop.zip transylvania.bostonmonks.com:/tmp/
+  # rm requirements.txt
   ansible-playbook -i devops/inventory.ini devops/playbooks/deploy.yml
 else
   echo You must be on release branch
