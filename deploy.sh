b5465fa1652b66b345a6858bda6f615ad3b67136
Alexandre Karpov <alexakarpov@gmail.com>
2023-04-25 12:43:01 -0400

    clean up deploy

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,10 @@
 BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 echo you are on $BRANCH
 
-if [ $BRANCH == 'main' ]; then
-  # find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
-  # rm -f htmshop.zip
-  # poetry export -f requirements.txt --with dev --without-hashes > requirements.txt
-  # zip -qr htmshop.zip ecommerce requirements.txt manage.py static templates
-  # scp htmshop.zip transylvania.bostonmonks.com:/tmp/
-  # rm requirements.txt
-  ansible-playbook -i devops/inventory.ini devops/playbooks/deploy.yml
-else
-  echo You must be on release branch
-  exit 1
-fi
+# find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+# rm -f htmshop.zip
+# poetry export -f requirements.txt --with dev --without-hashes > requirements.txt
+# zip -qr htmshop.zip ecommerce requirements.txt manage.py static templates
+# scp htmshop.zip transylvania.bostonmonks.com:/tmp/
+# rm requirements.txt
+ansible-playbook -i devops/inventory.ini devops/playbooks/deploy.yml
