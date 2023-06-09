ed2bb805b0dc3ef5f12cc8bcbfe43993071784c6
Alexandre Karpov <alexakarpov@gmail.com>
2023-06-09 00:29:10 -0400

    create the order upon payment success

--- ecommerce/apps/checkout/urls.py
+++ ecommerce/apps/checkout/urls.py
@@ -12,15 +12,8 @@ urlpatterns = [
         name="basket_update_delivery",
     ),
     path("delivery_address/", views.delivery_address, name="delivery_address"),
-    path(
-        "payment_selection/", views.payment_selection, name="payment_selection"
-    ),
+    path("payment_selection/", views.payment_selection, name="payment_selection"),
     #     path("payment_complete/", views.payment_complete, name="payment_complete"),
-    path(
-        "payment_successful/",
-        views.payment_successful,
-        name="payment_successful",
-    ),
     path("payment/", views.payment_with_token, name="token_payment"),
     path("guest_address/", views.guest_address, name="guest_address"),
 ]
