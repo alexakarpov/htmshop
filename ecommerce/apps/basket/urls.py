4e88f592399ee3314e11d73e08a429d05ea169e0
Alexandre Karpov <alexakarpov@gmail.com>
2023-06-16 14:11:31 -0400

    simplify basket urls

--- ecommerce/apps/basket/urls.py
+++ ecommerce/apps/basket/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "basket"
 
 urlpatterns = [
-    path("", views.basket_summary, name="basket_summary"),
-    path("add/", views.basket_add, name="basket_add"),
-    path("delete/", views.basket_delete, name="basket_delete"),
-    path("update/", views.basket_update, name="basket_update"),
+    path("", views.basket_summary, name="summary"),
+    path("add/", views.basket_add, name="add"),
+    path("delete/", views.basket_delete, name="delete"),
+    path("update/", views.basket_update, name="update"),
 ]
