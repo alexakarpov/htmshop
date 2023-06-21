9ba4b3e69a4d8f65a55e986c1ff6af888aaa7cc4
Alexandre Karpov <alexakarpov@gmail.com>
2023-06-21 08:41:19 -0400

    to info level aggain

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -36,14 +36,14 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "console": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.StreamHandler",
         },
     },
     "loggers": {
         "django": {
             "handlers": ["console"],
-            "level": "DEBUG",
+            "level": "INFO",
             "propagate": True,
         },
     },
