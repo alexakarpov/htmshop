b95af1e60f4165d76fdd6e009bb77f1fab1b2460
Alexandre Karpov <alexakarpov@gmail.com>
2023-07-24 23:06:21 -0400

    generate_prints now finished

--- ecommerce/apps/inventory/management/commands/generate_prints.py
+++ ecommerce/apps/inventory/management/commands/generate_prints.py
@@ -1,18 +1,24 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 
-from ecommerce.apps.inventory.models import Stock
+from ecommerce.apps.inventory.models import ProductStock, ProductType
+
+from ecommerce.constants import MOUNTED_ICON_TYPE_ID, ICON_PRINT_TYPE_ID
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        for p in Stock.objects.filter(
-                product_type__name="mounted icon"):
-            # print(f"--- working with {p.sku} ---")
-            print_sku = p.sku+'P'
-            try:
-                pr = Stock.objects.get(sku=print_sku)
-
-            except Stock.DoesNotExist:
-                print(f">>>> creating a new Inventory record: {p.sku}'s print SKU<<<")
-                
+        for p in ProductStock.objects.filter(
+            product_type=MOUNTED_ICON_TYPE_ID
+        ):
+            print_sku = p.sku + "P"
+            print_type = ProductType.objects.get(id=ICON_PRINT_TYPE_ID)
+            pr = ProductStock.objects.create(
+                sku=print_sku,
+                product=p.product,
+                product_type=print_type,
+                # restock_point=1,
+                target_amount=1,
+                weight=0.1,
+                price=18.0
+            )
