063298f1e0f3949eac5302ab7e49c5344f94f664
Alexandre Karpov <alexakarpov@gmail.com>
2023-07-26 21:51:18 -0400

    yank SKU RE to constants

--- ecommerce/constants.py
+++ ecommerce/constants.py
@@ -8,6 +8,7 @@ MOUNTED_ICON_TYPE_ID = 3
 ICON_PRINT_TYPE_NAME = "icon print"
 ICON_PRINT_TYPE_ID = 4
 LINES_PER_PAGE = 29
+SKU_REGEX="^A-(?!0)\d{1,3}(\.\d{1,2}x\d{1,2})?(?(1)[MP]|P?)\Z|^[BHJR]-(?!0)\d{1,3}\Z|^[DG]-(?!0)\d{1,3}P?\Z|^L-(?!0)\d{1,3}[ABC]\Z|^M-(?!0)\d{1,3}[AEJ]?\Z|^S-[1-9]\Z"
 
 ROOMS = [
     ("nowhere", "nowhere"),
