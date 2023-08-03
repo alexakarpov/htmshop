ac0995afcf59d2222c369871d97f5008001ce368
Alexandre Karpov <alexakarpov@gmail.com>
2023-08-02 22:02:08 -0400

    drop obsolete constants

--- ecommerce/constants.py
+++ ecommerce/constants.py
@@ -1,13 +1,8 @@
-PRODUCT_TYPE_BOOK = "book"
-PRODUCT_TYPE_MOUNTED_ICON = "mounted icon"
-TEST_ICON_SLUG = "holy_napkin"
-TEST_ICON2_SLUG = "burning_bush"
-TEST_BOOK_SLUG = "psalter"
-MOUNTED_ICON_TYPE_NAME = "mounted icon"
-MOUNTED_ICON_TYPE_ID = 3
 ICONS_CATEGORY_ID = 1
-ICON_PRINT_TYPE_NAME = "icon print"
 ICON_PRINT_TYPE_ID = 4
+MOUNTED_ICON_TYPE_ID = 3
+INCENSE_TYPE_ID = 2
+BOOK_TYPE_ID = 1
 LINES_PER_PAGE = 29
 SKU_REGEX="^A-(?!0)\d{1,3}(\.\d{1,2}x\d{1,2})?(?(1)[MP]|P?)\Z|^[BHJR]-(?!0)\d{1,3}\Z|^[DG]-(?!0)\d{1,3}P?\Z|^L-(?!0)\d{1,3}[ABC]\Z|^M-(?!0)\d{1,3}[AEJ]?\Z|^S-[1-9]\Z"
 
