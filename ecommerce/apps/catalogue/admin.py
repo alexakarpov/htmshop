871f187839fa6ea9ad73e5cd9183dbb979873285
Alexandre Karpov <alexakarpov@gmail.com>
2023-08-20 19:02:01 -0400

    another cat cleaning and admin customizarion

--- ecommerce/apps/catalogue/admin.py
+++ ecommerce/apps/catalogue/admin.py
@@ -1,5 +1,7 @@
+from typing import Any, List, Tuple
 from django import forms
 from django.contrib import admin
+import string
 
 from .models import (
     Category,
@@ -9,4 +11,32 @@ from .models import (
 from mptt.admin import MPTTModelAdmin
 
 admin.site.register(Category, MPTTModelAdmin)
-admin.site.register(Product)
+
+class ProductListFilter(admin.SimpleListFilter):
+    title = "Product Title Begins With Letter"
+    parameter_name = "startswith"
+
+    def lookups(self, request: Any, model_admin: Any) -> List[Tuple[Any, str]]:
+        return  [(x, x.upper()) for x in string.ascii_lowercase]
+    
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        print(f"building a queryset with {self.value()}")
+        if self.value():
+            return queryset.filter(
+                title__startswith=self.value()
+        )
+        else:
+            return queryset
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter=[ProductListFilter]
+
+
+admin.site.register(Product, ProductAdmin)
