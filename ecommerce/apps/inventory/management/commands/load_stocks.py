2ee6d1a60b5edc40344860cb87389c5bcf2e8701
Alexandre Karpov <alexakarpov@gmail.com>
2023-08-31 18:30:28 -0400

    BHJR stock import from csv, inventory fixture updated

--- ecommerce/apps/inventory/management/commands/load_stocks.py
+++ ecommerce/apps/inventory/management/commands/load_stocks.py
@@ -1,4 +1,4 @@
-import re, csv
+import re, csv, sys
 
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
@@ -7,6 +7,8 @@ from ecommerce.apps.catalogue.models import Product
 
 from ecommerce.apps.inventory.models import ProductType, Stock
 from ecommerce.constants import (
+    BHJR_RE,
+    BOOK_TYPE_ID,
     INCENSE_TYPE_ID,
     SKU_REGEX,
     NEW_RE,
@@ -20,8 +22,7 @@ icon_print = ProductType.objects.get(id=ICON_PRINT_TYPE_ID)
 incense_type = ProductType.objects.get(id=INCENSE_TYPE_ID)
 
 
-def process_row(row, pattern, stock_type):
-    print(f"processing: {row} with pattern {pattern}")
+def process_row(row, pattern: str, product_type=None):
     sku = row[0]
     m = re.match(pattern, sku)
     base = m.group(1)
@@ -31,17 +32,26 @@ def process_row(row, pattern, stock_type):
     restock_point = row[3]
     target_amount = row[4]
     price = row[5]
-    type = stock_type
+    type = None
+    try:
+        if not product_type:
+            stock_type_name = row[6]
+            type = ProductType.objects.get(name=stock_type_name)
+        else:
+            type = product_type
+    except ProductType.DoesNotExist:
+        sys.stderr.write(f"product type {stock_type_name} not found\n")
+
     try:
         catalogue_product = Product.objects.get(sku_base=base)
     except Product.DoesNotExist:
-        print(f"Product {base} not found")
+        sys.stderr.write(f"Product {base} not found\n")
         return -1
 
     try:
         stock = Stock.objects.create(
             product=catalogue_product,
-            product_type=stock_type,
+            product_type=type,
             spec=spec,
             sku=sku,
             restock_point=restock_point,
@@ -51,7 +61,7 @@ def process_row(row, pattern, stock_type):
         )
         print(f"created {stock}")
     except IntegrityError as e:
-        print(f"looks like a Stock record for {sku} already exists")
+        sys.stderr.write(f"looks like a Stock record for {sku} already exists\n")
 
 
 def load_icons_stock():
@@ -60,7 +70,16 @@ def load_icons_stock():
         next(r, None)  # skip the first row (header)
 
         for row in r:
-            process_row(row, NEW_RE, mounted_icon_type)
+            process_row(row, NEW_RE, product_type=mounted_icon_type)
+
+
+def load_BHJR_stock():
+    with open("ecommerce/apps/inventory/data/BHJR.csv") as f:
+        r = csv.reader(f)
+        next(r, None)  # skip the first row (header)
+
+        for row in r:
+            process_row(row, NEW_RE)
 
 
 def load_incense_stock():
@@ -69,7 +88,16 @@ def load_incense_stock():
         next(r, None)  # skip the first row (header)
 
         for row in r:
-            process_row(row, INCENSE_RE, incense_type)
+            process_row(row, INCENSE_RE, product_type=incense_type)
+
+
+def load_BHJR():
+    with open("ecommerce/apps/inventory/data/BHJR.csv") as f:
+        r = csv.reader(f)
+        next(r, None)  # skip the first row (header)
+
+        for row in r:
+            process_row(row, BHJR_RE)
 
 
 class Command(BaseCommand):
@@ -79,3 +107,4 @@ class Command(BaseCommand):
         """
         load_icons_stock()
         load_incense_stock()
+        load_BHJR()
