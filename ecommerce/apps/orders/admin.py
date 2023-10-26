f6049f5ca75625cd18b847c618ad3a8ba927181a
Alexandre Karpov <alexakarpov@gmail.com>
2023-10-26 17:22:33 -0400

    drop payments admin, hide addtocart alert box

--- ecommerce/apps/orders/admin.py
+++ ecommerce/apps/orders/admin.py
@@ -5,13 +5,13 @@ from rangefilter.filters import (
     DateRangeQuickSelectListFilterBuilder,
 )
 
-from .models import Order, OrderItem, Payment
+from .models import Order, OrderItem  # , Payment
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
-    readonly_fields = ['title']
+    readonly_fields = ["title","quantity"]
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -19,18 +19,16 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline,
     ]
-    list_filter = (
-        ("created_at", DateRangeQuickSelectListFilterBuilder()),
-    )
+    list_filter = (("created_at", DateRangeQuickSelectListFilterBuilder()),)
     search_fields = ("email", "full_name")
 
 
-class PaymentAdmin(admin.ModelAdmin):
-    # readonly_fields = ["order"]
-    search_fields = (
-        ("order__full_name",)
-    )
+# class PaymentAdmin(admin.ModelAdmin):
+#     # readonly_fields = ["order"]
+#     search_fields = (
+#         ("order__full_name",)
+#     )
 
 
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Payment, PaymentAdmin)
+# admin.site.register(Payment, PaymentAdmin)
