9c02a8a2f97ae3aaec7dc346c4acea0fb4599566
Alexandre Karpov <alexakarpov@gmail.com>
2024-01-09 22:20:30 -0500

    simplify the ifs chain

--- ecommerce/apps/catalogue/templatetags/prices.py
+++ ecommerce/apps/catalogue/templatetags/prices.py
@@ -13,17 +13,15 @@ register = template.Library()
 
 @register.simple_tag
 def get_price(customer: Account, stock: Stock):
-    if customer.is_anonymous:
+    if customer.is_anonymous or customer.is_bookstore == False:
         return stock.price
-    if customer.is_bookstore:
-        if (
-            customer.email in ANGEL_EMAILS
-            and stock.is_aseries()
-            and stock.sku.find("x") == -1
-        ):
-            return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_AL)
-        if stock.is_aseries() and stock.sku.find("x") == -1:
-            return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_A)
-        if stock.is_incense():
-            return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_I)
-    return stock.price
+    if (
+        customer.email in ANGEL_EMAILS
+        and stock.is_aseries() # not a print
+        and stock.sku.find("x") == -1 # standard size
+    ):
+        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_AL)
+    if stock.is_aseries() and stock.sku.find("x") == -1:
+        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_A)
+    if stock.is_incense():
+        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_I)
