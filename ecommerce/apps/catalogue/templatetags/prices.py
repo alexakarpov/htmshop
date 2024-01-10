b4d316bcb42b8de0a6f2bfb00ffe19dd719d0949
Alexandre Karpov <alexakarpov@gmail.com>
2024-01-09 23:22:16 -0500

    rework the custom tag to return a string with both prices

--- ecommerce/apps/catalogue/templatetags/prices.py
+++ ecommerce/apps/catalogue/templatetags/prices.py
@@ -14,14 +14,15 @@ register = template.Library()
 @register.simple_tag
 def get_price(customer: Account, stock: Stock):
     if customer.is_anonymous or customer.is_bookstore == False:
-        return stock.price
+        return f"{stock.price}"  # discounts not applicable to the customer
     if (
         customer.email in ANGEL_EMAILS
-        and stock.is_aseries() # not a print
-        and stock.sku.find("x") == -1 # standard size
+        and stock.is_aseries()  # not a print
+        and stock.sku.find("x") == -1  # standard size
     ):
-        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_AL)
+        return f"{stock.price - stock.percentage(BOOKSTORE_DISCOUNT_AL)} ( ${stock.price} )"
     if stock.is_aseries() and stock.sku.find("x") == -1:
-        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_A)
+        return f"{stock.price - stock.percentage(BOOKSTORE_DISCOUNT_A)} ( ${stock.price} )"
     if stock.is_incense():
-        return stock.price - stock.percentage(BOOKSTORE_DISCOUNT_I)
+        return f"{stock.price - stock.percentage(BOOKSTORE_DISCOUNT_I)} ( ${stock.price} )"
+    return f"{stock.price}"  # no discounts on the stock
