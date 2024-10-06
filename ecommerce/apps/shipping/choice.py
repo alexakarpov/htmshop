b9495010f025df5ca71804080e7b2ac472b5065b
Alexandre Karpov <alexakarpov@gmail.com>
2024-10-05 22:14:49 -0400

    fixing choices, addin service_code

--- ecommerce/apps/shipping/choice.py
+++ ecommerce/apps/shipping/choice.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from decimal import Decimal
+
 # from rest_framework import serializers
 
 
@@ -7,30 +8,31 @@ class ShippingChoiceSE:
     def __lt__(self, other):
         return self.price < other.price
 
-    def __init__(self, name, price, id, days):
-        self.name = name
+    def __init__(self, price, id, days, service_code):
         self.price = price
         self.id = id
         self.days = days
+        self.service_code = service_code
 
     def __eq__(self, other):
         if isinstance(other, ShippingChoiceSE):
             return (
-                self.name == other.name
+                self.service_code == other.service_code
                 and self.id == other.id
                 and self.price == other.price
                 and self.days == other.days
             )
         return False
 
+    @staticmethod
     def from_repr(it):
-        [name, price, id, days] = it.split("/")
-        return ShippingChoiceSE(name, Decimal(price), id, int(days))
+        [price, id, days, service_code] = it.split("/")
+        return ShippingChoiceSE(Decimal(price), id, int(days), service_code)
 
     # basket_update_delivery relies on this format!
 
     def __repr__(self):
-        return f"{self.name}/{self.price}/{self.id}/{self.days}"
+        return f"{self.price}/{self.id}/{self.days}/{self.service_code}"
 
 
 class ShippingChoiceSS:
@@ -59,26 +61,45 @@ class ShippingChoiceSS:
         return f"{self.serviceCode}/{self.price}"
 
 
-def rate_to_choice(rate_d):
+def rate_to_choice(rate_d: dict):
     return ShippingChoiceSE(
-        rate_d.get("service_code"),
         rate_d.get("shipping_amount").get("amount"),
         rate_d.get("rate_id"),
         rate_d.get("delivery_days"),
+        rate_d.get("service_code"),
     )
 
 
-def split_tiers_SE(choices, international=False):
-    if international:
-        regular = [x for x in choices if x.name in settings.INTL_REGULAR]
-        express = [x for x in choices if x.name in settings.INTL_EXPRESS]
-        fast = [x for x in choices if x.name in settings.INTL_FAST]
-    else:
-        regular = [x for x in choices if x.name in settings.REGULAR]
-        express = [x for x in choices if x.name in settings.EXPRESS]
-        fast = [x for x in choices if x.name in settings.FAST]
+def split_tiers_SE(
+    choices, international=False
+) -> dict[str, list[ShippingChoiceSE]]:
 
-    return {"regular": regular, "express": express, "fast": fast}
+    regular_tier = []
+    fast_tier = []
+    express_tier = []
+
+    regular_codes = settings.REGULAR
+    fast_codes = settings.FAST
+
+    if international:
+        regular_codes = settings.INTL_REGULAR
+        fast_codes = settings.INTL_FAST
+
+    for choice in choices:
+        if choice.service_code in regular_codes:
+            regular_tier.append(choice)
+            continue
+        elif choice.service_code in fast_codes:
+            fast_tier.append(choice)
+            continue
+        else:  # express
+            express_tier.append(choice)
+
+    return {
+        "regular": regular_tier,
+        "fast": fast_tier,
+        "express": express_tier,
+    }
 
 
 def split_tiers_SS(choices, international=False):
