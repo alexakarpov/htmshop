cbbd3093bed501a9a400e71177944baf1136b2fa
Alexandre Karpov <alexakarpov@gmail.com>
2024-11-08 19:47:10 -0500

    cleanup

--- ecommerce/apps/catalogue/views.py
+++ ecommerce/apps/catalogue/views.py
@@ -1,5 +1,4 @@
 import logging
-#from datetime import datetime
 
 from django.shortcuts import get_object_or_404, render, redirect
 
@@ -8,9 +7,7 @@ from ecommerce.constants import ID_LOOKUP
 
 from .models import Category, Product
 
-# logger = logging.getLogger(__name__)
 logger = logging.getLogger(__name__)
-
 
 def catalogue_index(request):
     featured = Product.objects.filter(is_active=True, is_featured=True)
@@ -30,15 +27,6 @@ def legacy_product(request, legacy_id, ignored=None):
         return redirect("catalogue:home")
 
 
-def catalogue_new(request):
-    products = Product.objects.filter(is_active=True)
-    return render(
-        request,
-        "catalogue/wip_index.html",
-        {"products": products, "categories": Category.objects.all()},
-    )
-
-
 def category_list(request, category_slug=None, letter=None):
     cat = get_object_or_404(Category, slug=category_slug)
 
@@ -50,7 +38,6 @@ def category_list(request, category_slug=None, letter=None):
         {
             "category": cat,
             "products": products,
-            "categories": Category.objects.all(),
         },
     )
 
@@ -65,24 +52,20 @@ def saints_filtered(request, letter=None):
         {
             "category": saints,
             "products": saints_filtered,
-            "categories": Category.objects.all(),
         },
     )
 
 
 def product_detail(request, slug):
-
-    logger.warning(f"request came for {slug} details page")
-
     referrer = request.META.get("HTTP_REFERER")
 
     product = get_object_or_404(Product, slug=slug, is_active=True)
     skus = product.get_skus()
 
-    logger.warning(f"fetched {len(skus)} skus:")
+#    logger.warning(f"fetched {len(skus)} skus:")
 
-    for s in skus:
-        logger.warning(s)
+#    for s in skus:
+#        logger.warning(s)
 
     return render(
         request,
@@ -91,7 +74,6 @@ def product_detail(request, slug):
             "product": product,
             "skus": skus,
             "referred": referrer,
-            "categories": Category.objects.all(),
         },
     )
 
