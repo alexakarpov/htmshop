86a259cfc23f4174bc354b76034d231a21ae01ab
Alexandre Karpov <alexakarpov@gmail.com>
2024-11-27 18:04:19 -0500

    tieup

--- ecommerce/apps/checkout/tests/test_checkout.py
+++ ecommerce/apps/checkout/tests/test_checkout.py
@@ -2,11 +2,9 @@ from django.test import TestCase
 from ecommerce.apps.basket.basket import Basket
 from ecommerce.apps.checkout.views import classify_order_add_items
 
+
 class CheckoutTest(TestCase):
-    fixtures = [
-        "catalogue.json",
-        "test_inventory.json"
-    ]
+    fixtures = ["catalogue.json", "test_inventory.json"]
 
     def test_item_set(self):
-        assert(False)
+        self.assertFalse(True,msg="Finish the test")
--- ecommerce/apps/checkout/urls.py
+++ ecommerce/apps/checkout/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     ),
     path("delivery_address/", views.delivery_address, name="delivery_address"),
     path("payment_selection/", views.payment_selection, name="payment_selection"),
-    #     path("payment_complete/", views.payment_complete, name="payment_complete"),
     path("payment/", views.payment_with_token, name="token_payment"),
     path("pay_later/", views.pay_later, name="pay_later"),
     path("guest_address/", views.guest_address, name="guest_address"),
